875f03f1ea5a224f24f7fdf6691f20d3c4cf1b3f
crative <hannig.sebi@gmail.com>
2024-07-03 19:10:45 +0200

    go lsp and fromating

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -7,6 +7,14 @@ M.dap = {
       "<cmd> DapToggleBreakpoint <CR>",
       "Add breakpoint at line"
     },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require("dap.ui.widgets");
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar",
+    },
     ["<leader>dr"] = {
       "<cmd> DapContinue <CR>",
       "Start or continue the debugger",
@@ -14,8 +22,46 @@ M.dap = {
   }
 }
 
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require("dap-go").debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require("dap-go").debug_last()
+      end,
+      "Debug go last"
+    },
+
+  }
+}
+
 vim.api.nvim_command('command! UpperConsole split | resize 10 | terminal')
 vim.api.nvim_command('command! RelLine lua vim.opt.relativenumber = true')
 vim.api.nvim_command('command! RelLineOff lua vim.opt.relativenumber = false')
+vim.api.nvim_command('command! ErrGo lua InsertGoErrSnippet()')
+
+function InsertGoErrSnippet()
+  local lines = {
+    "if err != nil {",
+    "  log.Fatal(err)",
+    "}"
+  }
+
+  -- Get the current buffer and cursor position
+  local buf = vim.api.nvim_get_current_buf()
+  local pos = vim.api.nvim_win_get_cursor(0)
+
+  -- Insert the lines at the current cursor position
+  vim.api.nvim_buf_set_lines(buf, pos[1], pos[1], false, lines)
+end
+
+
+
 
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -69,6 +69,25 @@ local plugins = {
     end
   },
   {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end
+  },
+  {
     "jose-elias-alvarez/null-ls.nvim",
     event = "VeryLazy",
     opts = function()
